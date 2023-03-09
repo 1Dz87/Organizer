f34ea69bf1fc89e8232fa4c4ce45bc8ed3139b1c
1Dz <kulin.mks@yandex.ru>
2023-03-09 10:03:36 +0300

    Added authorization to openApi

--- src/main/java/by/itstep/organizaer/config/OpenApiConfig.java
+++ src/main/java/by/itstep/organizaer/config/OpenApiConfig.java
@@ -1,17 +1,35 @@
 package by.itstep.organizaer.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@SecurityScheme(
-        type = SecuritySchemeType.APIKEY,
-        name = "basicAuth",
-        scheme = "basic",
-        in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
 
+    private static final String AUTH_SCHEME = "Authorization";
 
+    @Bean
+    public OpenAPI openAuth() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(AUTH_SCHEME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList(AUTH_SCHEME)))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Organizer");
+    }
 }
